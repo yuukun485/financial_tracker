1e9a8d7998f3ec5023bdf6d4d249e498bf77700a
yuukun485 <144216065+yuukun485@users.noreply.github.com>
2025-08-30 13:21:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ st.set_page_config(
     page_title="Personal Finance Tracker",
     page_icon=":moneybag:",
     layout="centered",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapesd"
 )
 
 # st.session_stateの初期化 (変更点1)
